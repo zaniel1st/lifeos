c170efc2bf91f72a03bc6320ec5532d0abb8fb50
Kenawak Ephrem <anekkena111@gmail.com>
2026-07-10 00:36:43 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { URL, fileURLToPath } from 'url'
 export default defineConfig({
   plugins: [react()],
   base: '/lifeos/',
+  // If your files are nested inside a subfolder, this tells Vite to find them:
+  root: './', 
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
