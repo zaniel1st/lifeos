a6bf496095937d30b0e582a709a3419a6da10483
Kenawak Ephrem <anekkena111@gmail.com>
2026-07-10 00:28:29 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { URL, fileURLToPath } from 'url'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  base: '/lifeos/', // ◄ Add this line here (make sure it matches your repo name)
+  plugins: [react()],
+  base: '/lifeos/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
